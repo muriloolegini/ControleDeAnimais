0198ba425e302e4234ab03b8dc6051e277dc8e00
Murilo Olegini <muriloolegini@outlook.com>
2022-10-26 09:48:38 -0300

    Criando e percorrendo o Array

--- Program.cs
+++ Program.cs
@@ -2,41 +2,59 @@
 using static ControleDeAnimais.Animal;
 
 
-Animal animal1 = new Animal();
+Animal animal;
+int quantidade = 0;
+
+// Array
+Animal[] animais = new Animal[30];
 
 int totalCachorro = 0, totalGato = 0, totalPeixe = 0;
 int tipoAnimal = 0;
 
 Console.WriteLine("Controle de animais");
 
-
-// Primeiro animal
-Console.Write("Informe o nome do 1º animal: ");
-animal1.Nome = Console.ReadLine();
-Console.Write("Informe o tipo do 1º animal (Cachorro - 0, Gato - 1, Peixe - 2): ");
-
+// Leitura da quantidade de animais
 try
 {
-    tipoAnimal = Convert.ToInt32(Console.ReadLine());
-    if (tipoAnimal < 0) tipoAnimal = 2;
-    if (tipoAnimal > 2) tipoAnimal = 2;
+    Console.WriteLine("Quantos animais deseja informar (máximo 30): ");
+    quantidade = Convert.ToInt32(Console.ReadLine());
+
+    if (quantidade > 20) quantidade = 20;
 }
 catch
 {
-    tipoAnimal = 2;
+    quantidade = 0;
 }
 
-// if (tipoAnimal == 0) animal1.Tipo = TipoAnimal.Cachorro;
-// if (tipoAnimal == 1) animal1.Tipo = TipoAnimal.Gato;
-// if (tipoAnimal == 2) animal1.Tipo = TipoAnimal.Peixe;
+for (int i = 0; i < quantidade; i++)
+{
+    // Leitura do animal
+    animal = new Animal();
+    Console.Write($"Informe o nome do {i + 1}º animal: ");
+    animal.Nome = Console.ReadLine();
+    Console.Write($"Informe o tipo do {i + 1}º animal (Cachorro - 0, Gato - 1, Peixe - 2): ");
 
-// Tipo informado pelo usuário
-animal1.Tipo = (TipoAnimal)tipoAnimal;
+    try
+    {
+        tipoAnimal = Convert.ToInt32(Console.ReadLine());
+        if (tipoAnimal < 0) tipoAnimal = 2;
+        if (tipoAnimal > 2) tipoAnimal = 2;
+    }
+    catch
+    {
+        tipoAnimal = 2;
+    }
 
-// Contando o tipo informado
-if (animal1.Tipo == TipoAnimal.Cachorro) totalCachorro++;
-if (animal1.Tipo == TipoAnimal.Gato) totalGato++;
-if (animal1.Tipo == TipoAnimal.Peixe) totalPeixe++;
+    animal.Tipo = (TipoAnimal)tipoAnimal;
+    // Contando o tipo informado
+    if (animal.Tipo == TipoAnimal.Cachorro) totalCachorro++;
+    if (animal.Tipo == TipoAnimal.Gato) totalGato++;
+    if (animal.Tipo == TipoAnimal.Peixe) totalPeixe++;
+
+    // Armazenar animal no vetor
+    animais[i] = animal;
+}
+
 
 // Resultado
 System.Console.WriteLine("Total de animais");
